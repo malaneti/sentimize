ef88ee76a632432dde2e4fbcd80c9c2b0c886366
Brett <brett@applogix.com>
2016-06-08 12:29:38 -0700

    Refactor TeamMember component to stateless functional component

--- client/components/team-view/TeamMember.jsx
+++ client/components/team-view/TeamMember.jsx
@@ -1,33 +1,21 @@
 import React from 'react';
 import { browserHistory } from 'react-router';
 
-export default class TeamMember extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  showSessionReport() {
-    browserHistory.push('/reports/' + this.props.sessionId.toString());
-  }
-
-  render() {
-    return (
-      <div className="team-member-block" onClick={this.showSessionReport.bind(this)}>
-        {/*<div className="team-member-title">{this.props.entry.title}</div>
-        <div className="team-member-description">{this.props.entry.description}</div>
-        <div className="team-member-subject">
-          <span className="label">Subject: </span>
-          <span className="value">{this.props.entry.subject}</span>
-        </div>
-        <div className="team-member-date">
-          <span className="label">Date: </span>
-          <span className="value">{this.props.entry.date}</span>
-        </div>
-        <div className="team-member-duration">
-          <span className="label">Duration: </span>
-          <span className="value">{this.props.entry.duration} seconds</span>
-        </div>*/}
-      </div>
-    )
-  }
-};
+export default TeamMember = props => (
+  <div className="team-member-block" onClick={props.showSessionReport}>
+    {/*<div className="team-member-title">{this.props.entry.title}</div>
+    <div className="team-member-description">{this.props.entry.description}</div>
+    <div className="team-member-subject">
+      <span className="label">Subject: </span>
+      <span className="value">{this.props.entry.subject}</span>
+    </div>
+    <div className="team-member-date">
+      <span className="label">Date: </span>
+      <span className="value">{this.props.entry.date}</span>
+    </div>
+    <div className="team-member-duration">
+      <span className="label">Duration: </span>
+      <span className="value">{this.props.entry.duration} seconds</span>
+    </div>*/}
+  </div>
+);
--- client/components/team-view/TeamView.jsx
+++ client/components/team-view/TeamView.jsx
@@ -9,7 +9,7 @@ export default class TeamView extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      userSessions: {}
+      userSessions: []
     }
   }
 
@@ -33,13 +33,27 @@ export default class TeamView extends React.Component {
     });
   }
 
+  showSessionReport() {
+    console.log('showSessionReport function needs updating for correct REACT ROUTER PATH');
+    // browserHistory.push('/reports/' + this.props.sessionId.toString());
+  }
+
   _sessionsByUser(sessions) {
+    //Build an object with user sessions in an array by user
     let users = sessions.reduce(function(usersSoFar, session) {
       usersSoFar[session.userId] = usersSoFar[session.userId].push(session) || [session];
       return usersSoFar
      },{});
 
-    return users;
+    //Convert object to array of user objects & related sessions
+    let userSessions = [];
+    for (let user of users) {
+      userSessions.push({
+        user: user,
+        sessions: users[user]
+      })
+    }
+    return userSessions;
   }
 
   render() {
@@ -47,10 +61,13 @@ export default class TeamView extends React.Component {
       <div className="view team-view">
         <h4 className="team-view-title">Team Dashboard</h4>
         <div className="pure-g">
-          {this.state.sessionBuckets.map(
-            entry => (
+          {this.state.userSessions.map(
+            user => (
               <div className="pure-u-1-3">
-                <TeamMember entry={entry} sessionId={entry.id} />
+                <TeamMember userName={user.sessions[0].firstName + ' ' + user.sessions[0].lastName} 
+                            userId={user.user} 
+                            sessions={user.sessions} 
+                            onClick={this.showSessionReport.bind(this)}/>
               </div>
             )
           )}
