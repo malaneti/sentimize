2a2cbc818f37f53522e651c9f02ae8f3f05f2e8c
Brett <brett@applogix.com>
2016-06-08 18:01:59 -0700

    Sundry minor changes to TeamView and TeamMember

--- client/components/team-view/TeamMember.jsx
+++ client/components/team-view/TeamMember.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { browserHistory } from 'react-router';
 
 const TeamMember = props => (
-  <div className="team-member-row" onClick={props.showSessionReport}>
+  
+  <div className="team-member-row" onClick={props.showUserSessions}>
     <div className="team-member-userName">{props.user.userName}</div>
     {/*<div className="team-member-description">{props.entry.description}</div>
     <div className="team-member-subject">
@@ -13,7 +14,7 @@ const TeamMember = props => (
       <span className="label">Date: </span>
       <span className="value">{props.entry.date}</span>
     </div>
-    <div className="team-member-duration">
+    <div className="team-member-duration">7ujn 
       <span className="label">Duration: </span>
       <span className="value">{props.entry.duration} seconds</span>
     </div>*/}
