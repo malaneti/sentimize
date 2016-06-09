f90b920b05415aafd9eb0a0a3a3ab449a17ede66
Brett <brett@applogix.com>
2016-06-08 21:05:27 -0700

    Change SessionsView to stateful class component

--- client/components/sessions-view/SessionsView.jsx
+++ client/components/sessions-view/SessionsView.jsx
@@ -5,21 +5,48 @@ import $ from 'jquery';
 
 // import dummyData from './../../../data/session-data.json';
 
-const SessionsView = props => (
-  <div className="view sessions-view">
-    <h4 className="sessions-view-title">My Sessions</h4>
-    <div className="pure-g">
-      {props.sessions.map(
-        session => (
-          <div className="pure-u-1-3">
-            <SessionEntry session={session} 
-                          sessionId={session.id}
-                          onClick={props.showSessionReport} />
-          </div>
-        )
-      )}
-    </div>
-  </div>
-);
+export default class SessionsView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sessionEntries: []
+    }
+  }
 
-export default SessionsView;
+  componentDidMount() {
+    this._getSessions(function(data) {
+      this.setState({ sessionEntries: data });
+    }.bind(this));
+  }
+
+  _getSessions(callback) {
+    $.ajax({
+      method: 'GET',
+      url: '/api/session',
+      success: function(data) {
+        callback(data);
+      },
+      error: function(error) {
+        console.error('_getSessions Error:', error);
+      },
+      dataType: 'json'
+    });
+  }
+
+  render() {
+    return (
+      <div className="view sessions-view">
+        <h4 className="sessions-view-title">My Sessions</h4>
+        <div className="pure-g">
+          {this.state.sessionEntries.map(
+            entry => (
+              <div className="pure-u-1-3">
+                <SessionEntry entry={entry} sessionId={entry.id} />
+              </div>
+            )
+          )}
+        </div>
+      </div>
+    )
+  }
+}
