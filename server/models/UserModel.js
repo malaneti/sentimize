02dcf5998fdb81c82b6d00513bc30e7313e04d0b
Gabriel F <gfcodes@gmail.com>
2016-06-09 16:29:00 -0700

    Revert "Updated Team Controller in DB"

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -1,21 +1,20 @@
 var db = require('../config/db');
 var Snapshot = require('./SnapshotModel.js');
 var Session = require('./SessionModel.js');
+var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
-var Team = require('./TeamModel.js')
 
 db.knex.schema.hasTable('users').then(function(exists) {
   if (!exists) {
     db.knex.schema.createTable('users', function (user) {
       user.increments('id').primary();
       user.string('email', 255).unique();
-      user.string('username', 255);
+      user.string('password', 255);
       user.string('gender', 1);
       user.integer('age');
       user.string('ethnicity', 255);
       user.string('firstName', 255);
       user.string('lastName', 255);
-      user.string('teamid', 255)
       user.timestamps();
     }).then(function (table) {
       console.log('Created Table', table);
@@ -33,8 +32,22 @@ var User = db.Model.extend({
     return this.hasMany(Session);
   },
 
- team: function() {
-    return this.belongTo(Team, 'teamid');
+  initialize: function() {
+    this.on('creating', this.hashPassword);
+  },
+
+  comparePassword: function(attemptedPassword, callback) {
+    bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
+      callback(isMatch);
+    });
+  },
+
+  hashPassword: function() {
+    var cipher = Promise.promisify(bcrypt.hash);
+    return cipher(this.get('password'), null, null).bind(this)
+      .then(function(hash) {
+        this.set('password', hash);
+      });
   }
 
 });
