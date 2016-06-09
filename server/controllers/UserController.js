3257687cda7b7a5139f1de78b08debc85ef55065
Mala Neti <mneti91@gmail.com>
2016-06-09 12:13:33 -0700

    updated UserController

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -7,7 +7,7 @@ exports.createUser = function(req, res) {
     firstName: req.body.firstname,
     lastName: req.body.lastname,
     email: req.body.email,
-    password: req.body.password
+    username: req.body.username
   };
 
   User.where('email', userObj.email).fetch().then(function(user) {
@@ -34,50 +34,6 @@ exports.getCurrentUser = function(req, res) {
     })
 };
 
-exports.updateUser = function(req, res) {
-  if (req.body.hasNewPassword === 'true') {
-    setNewPassword(req, res);
-  } else if (req.body.hasNewPassword === 'false') {
-    updateUserProfile(req, res);
-  }
-};
 
-var updateUserProfile = function(req, res) {
-  var updatedUser = req.body;
-  delete req.body.hasNewPassword;
 
-  new User({ id: req.user.id }).save(updatedUser)
-    .then(function(updatedUser) {
-      res.status(200).send(updatedUser);
-    })
-    .catch(function(err) {
-      console.error(err);
-    })
-};
-
-var setNewPassword = function(req, res) {
-  User.where({ id: req.user.id }).fetch()
-    .then(function(currentUser) {
-      currentUser.comparePassword(req.body.currentPassword, function(isMatch) {
-        if (!isMatch) {
-          res.status(401).send({ message: 'Incorrect password.' });
-        } else {
-          Promise.promisify(bcrypt.hash)(req.body.newPassword, null, null)
-            .then(function(hashedPassword) {
-              return new User({ id: req.user.id }).save({ password: hashedPassword });
-            })
-            .then(function(updatedUser) {
-              updatedUser.hashPassword();
-              res.status(200).send(updatedUser);
-            })
-            .catch(function(err) {
-              console.error(err);
-            })
-        }
-      });
-    })
-    .catch(function(err) {
-      console.error(err);
-    })
-};
 
